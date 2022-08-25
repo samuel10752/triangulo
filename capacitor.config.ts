f1e427aaab215f8109e9d57232644ac5aa9c3943
Samuel <samuelcauabarbosa114@gmail.com>
2022-08-25 08:52:04 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'triangulo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
