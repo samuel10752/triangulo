be05e8f4eab221100e4f1f1f15d64db33dde8190
Samuel <samuelcauabarbosa114@gmail.com>
2022-08-25 11:30:08 -0300

    Atalização dia 25/08/2022 as 11:30

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss','../explore-container/explore-container.component.scss']
 })
 export class Tab1Page {
-
+  num1 : string
+  num2 : string
+  num3 : string
+  saida : string
   constructor() {}
 
+  analisa(){
+  var a = parseFloat(this.num1)
+  var b = parseFloat(this.num2)
+  var c = parseFloat(this.num3)
+
+  if ((a + b) > c && (a + c) > b && (b + c) > a){
+    if ((a != b) && (a != c) && (b !=c)){
+      this.saida = " Triangulo  Escaleno"
+      }
+
+    if ((a == b) && (a == c) && (b == c)) {
+      this.saida = " Triangulo Equilatero"  
+    }
+
+    else{
+      this.saida = " Triangulo Isosceles"
+    }
+  }
+
+  else{
+    this.saida  = "Triangulo Não Existe"
+  }
 }
+}
+
