abdff446595b83dee8d980b4012154d67fb08582
Samuel <samuelcauabarbosa114@gmail.com>
2022-08-26 10:39:00 -0300

    Atualização dia 26/08 as 10:38

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,29 +7,37 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['tab2.page.scss','../explore-container/explore-container.component.scss']
 })
 export class Tab2Page {
-  
-  Base: string
+
+  Base : string
   Altura : string
-  saida : string
+  img : any = "assets/icon/calculo.png"
+  resultado : any
 
   constructor(private alertController: AlertController) {}
-
-  async presentAlert(op:string, nome: string) {
-    const alert = await this.alertController.create({
-      header: 'Area',
-      subHeader: '',
-      message: resultado,
-      buttons: ['FECHAR'],
-    });
-
-    await alert.present();
-  }
+    
+    async presentAlert(result) {
+      const alert = await this.alertController.create({
+        header: "Area",
+        subHeader: '',
+        message: result,
+        buttons: ['FECHAR'],
+  });
+  await alert.present();
+}
 
   analisar(){
-  var b = parseFloat(this.Base)
-  var h = parseFloat(this.Altura)
-  if ((a + b) > c && (a + c) > b && (b + c) > a){
-    
+    var b = parseFloat(this.Base)
+    var h = parseFloat(this.Altura)
+    if ((b != 0 && h != 0) && (b + h >= 0) || (b - h <= 0)){
+      this.resultado = (b*h)/2
+      var result = this.resultado
+      this.img = "assets/icon/fogos-de-artificio.gif"
+      this.presentAlert(result)
     }
-  }
+    else{
+      result = "Triangulo inexitente"
+      this.presentAlert(result)
+      this.img = "assets/icon/alerta.png"
+    }
+}
 }
